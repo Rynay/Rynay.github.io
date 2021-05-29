dc677f316573d5ca77e44016a00f90828cb75b25
Rynay <feirs911@gmail.com>
2021-05-29 12:31:16 +0300

    basic layout and fonts

---
+++ src/components/Header/Header.js
@@ -0,0 +1,5 @@
+const Header = () => {
+  return <header className="header">Hello</header>;
+};
+
+export default Header;
---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,13 @@
+const Sidebar = () => {
+  return (
+    <section className="sidebar">
+      <h1>Madison Marshall</h1>
+      <p>Frontend Developer</p>
+      <div>
+        <img src="/images/avatar.jpg" alt="" />
+      </div>
+    </section>
+  );
+};
+
+export default Sidebar;
