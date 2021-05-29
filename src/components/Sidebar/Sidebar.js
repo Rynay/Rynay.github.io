00ec7a2ff28b32d1e14e7386c93f208e9b702f69
Rynay <feirs911@gmail.com>
2021-05-29 13:21:20 +0300

    sidebar done

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,10 +1,53 @@
+import { FaInfo } from 'react-icons/fa';
+
 const Sidebar = () => {
   return (
     <section className="sidebar">
-      <h1>Madison Marshall</h1>
-      <p>Frontend Developer</p>
-      <div>
+      <div className="sidebar__header">
+        <h1 className="sidebar__name">Madison Marshall</h1>
+        <p className="sidebar__job">Frontend Developer</p>
+      </div>
+      <div className="sidebar__imageContainer">
         <img src="/images/avatar.jpg" alt="" />
+      </div>
+      <div className="sidebar__info">
+        <span className="sidebar__iconContainer">
+          <FaInfo className="sidebar__icon" />
+        </span>
+        <ul className="sidebar__list">
+          <li
+            aria-hidden
+            style={{ color: 'transparent' }}
+            className="sidebar__list-item"
+          >
+            .
+          </li>
+          <li className="sidebar__list-item">
+            <div>
+              Name: <span>Madison Marshall</span>
+            </div>
+          </li>
+          <li className="sidebar__list-item">
+            <div>
+              Birthday: <span>14 february 2001</span>
+            </div>
+          </li>
+          <li className="sidebar__list-item">
+            <div>
+              Job: <span>Student</span>
+            </div>
+          </li>
+          <li className="sidebar__list-item">
+            <div>
+              Email: <span>Email@gmail.com</span>
+            </div>
+          </li>
+          <li className="sidebar__list-item">
+            <div>
+              Skype: <span>Madison Marshall</span>
+            </div>
+          </li>
+        </ul>
       </div>
     </section>
   );
