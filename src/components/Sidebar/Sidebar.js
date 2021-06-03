b3774416c1fcfdb8e66fa336c0d33b9583073397
Rynay <feirs911@gmail.com>
2021-06-03 14:40:58 +0300

    sidebar fully depends on firebase

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,7 +1,8 @@
 import { FaInfo } from 'react-icons/fa';
 import { useLayoutEffect, useState } from 'react';
+import { connect } from 'react-redux';
 
-const Sidebar = ({ width }) => {
+const Sidebar = ({ width, avatar, info = {} }) => {
   const [style, setStyle] = useState({
     top: '5rem',
     left: `${(width / 2 - 800 / 2) / 10}rem`,
@@ -18,11 +19,11 @@ const Sidebar = ({ width }) => {
   return (
     <section style={style} className="sidebar">
       <div className="sidebar__header">
-        <h1 className="sidebar__name">Madison Marshall</h1>
-        <p className="sidebar__job">Frontend Developer</p>
+        <h1 className="sidebar__name">{info.title}</h1>
+        <p className="sidebar__job">{info.subtitle}</p>
       </div>
       <div className="sidebar__imageContainer">
-        <img src="/images/avatar.jpg" alt="" />
+        <img src={avatar} alt="" />
       </div>
       <div className="sidebar__info">
         <span className="sidebar__iconContainer">
@@ -36,35 +37,23 @@ const Sidebar = ({ width }) => {
           >
             .
           </li>
-          <li className="sidebar__list-item">
-            <div>
-              Name: <span>Madison Marshall</span>
-            </div>
-          </li>
-          <li className="sidebar__list-item">
-            <div>
-              Birthday: <span>14 february 2001</span>
-            </div>
-          </li>
-          <li className="sidebar__list-item">
-            <div>
-              Job: <span>Student</span>
-            </div>
-          </li>
-          <li className="sidebar__list-item">
-            <div>
-              Email: <span>Email@gmail.com</span>
-            </div>
-          </li>
-          <li className="sidebar__list-item">
-            <div>
-              Skype: <span>Madison Marshall</span>
-            </div>
-          </li>
+
+          {info.list?.map((field) => (
+            <li key={field.title} className="sidebar__list-item">
+              <div>
+                {field.title}: <span>{field.value}</span>
+              </div>
+            </li>
+          ))}
         </ul>
       </div>
     </section>
   );
 };
 
-export default Sidebar;
+const mapStateToProps = (state) => ({
+  avatar: state.data?.avatar,
+  info: state.data?.info,
+});
+
+export default connect(mapStateToProps)(Sidebar);
