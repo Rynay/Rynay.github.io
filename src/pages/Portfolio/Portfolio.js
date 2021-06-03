0ef701fce49988312b96702d0b0e6e78dd076722
Rynay <feirs911@gmail.com>
2021-06-03 17:14:19 +0300

    portfolio filters

--- src/pages/Portfolio/Portfolio.js
+++ src/pages/Portfolio/Portfolio.js
@@ -1,76 +1,62 @@
-import { forwardRef, useState } from 'react';
+import { connect } from 'react-redux';
+import { forwardRef, useState, useEffect } from 'react';
 import FlipMove from 'react-flip-move';
+import { FaArrowDown, FaArrowUp } from 'react-icons/fa';
 
-const projects = [
-  {
-    id: 1,
-    title: 'abc',
-    description: 'dfghjk',
-    technologies: ['React', 'Redux', 'Firebase'],
-  },
-  {
-    id: 2,
-    title: '123',
-    description: 'dfghjk',
-    technologies: ['React', 'Redux', 'TypeScript'],
-  },
-  {
-    id: 3,
-    title: '789',
-    description: 'dfghjk',
-    technologies: ['React', 'Firebase', 'TypeScript'],
-  },
-  {
-    id: 4,
-    title: '123456',
-    description: 'dfghjk',
-    technologies: ['React', 'Redux', 'Firebase'],
-  },
-  {
-    id: 5,
-    title: '789456',
-    description: 'dfghjk',
-    technologies: ['React', 'Redux', 'TypeScript'],
-  },
-];
-
-const Project = forwardRef((props, ref) => <li ref={ref}>{props.title}</li>);
-
-const ProjectsList = ({ projects, options, dependencies }) => {
+const Project = forwardRef((props, ref) => {
   return (
-    <ul>
-      <FlipMove>
+    <li ref={ref} className="project">
+      <a className="project__link" href={props.link}>
+        <div className="project__imageContainer">
+          <img src={props.image} alt="Site example" />
+        </div>
+        <div className="project__info">
+          <h3 className="project__title">{props.title}</h3>
+          <p className="project__description">{props.description}</p>
+        </div>
+      </a>
+    </li>
+  );
+});
+
+const ProjectsList = ({ projects = [], options = [], dependencies }) => {
+  return (
+    <FlipMove>
+      <ul className="projects-list">
         {projects
           .filter(options.filter(dependencies))
           .sort(options.sort)
           .map((project) => (
             <Project key={project.id} {...project} />
           ))}
-      </FlipMove>
-    </ul>
+      </ul>
+    </FlipMove>
   );
 };
 
-const Portfolio = () => {
+const Portfolio = ({ portfolio }) => {
   const [filterDependencies, setFilterDependencies] = useState([]);
   const [options, setOptions] = useState({
     sortedIn: 'Asc',
-    sort: (a, b) => a.id - b.id,
+    sort: (a, b) => a.date - b.date,
     filter: (dependencies) => (t) =>
       dependencies.every((dep) => t.technologies.includes(dep)),
   });
+  const [listOfPossibleTechnologies, setListOfPossibleTechnologies] = useState(
+    []
+  );
   const sortDesc = () => {
     setOptions((options) => ({
       ...options,
       sortedIn: 'Desc',
-      sort: (a, b) => b.id - a.id,
+      sort: (a, b) => b.date - a.date,
     }));
   };
   const sortAsc = () => {
     setOptions((options) => ({
       ...options,
       sortedIn: 'Asc',
-      sort: (a, b) => a.id - b.id,
+      sort: (a, b) => a.date - b.date,
     }));
   };
   const toggleFilterDependencies = (dependency) => {
@@ -81,15 +67,13 @@ const Portfolio = () => {
     }
   };
 
-  const listOfPossibleTechnologies = [
-    'React',
-    'Redux',
-    'TypeScript',
-    'Styled Components',
-    'Material UI',
-    '[CSS / SCSS] Modules',
-    'Firebase',
-  ];
+  useEffect(() => {
+    if (!portfolio) return;
+
+    setListOfPossibleTechnologies([
+      ...new Set(portfolio.flatMap((item) => item.technologies)),
+    ]);
+  }, [portfolio]);
 
   return (
     <>
@@ -97,28 +81,60 @@ const Portfolio = () => {
         <h2 className="title">
           <span>Portfolio</span>
         </h2>
-        <div>
-          <button onClick={options.sortedIn === 'Asc' ? sortDesc : sortAsc}>
-            Sort By Date {options.sortedIn === 'Asc' ? '\u2191' : '\u2193'}
-          </button>
-          {listOfPossibleTechnologies.map((tech) => (
+        <div className="options">
+          <div className="options__left">
             <button
-              key={tech}
-              onClick={() => {
-                toggleFilterDependencies(tech);
-              }}
-              onKeyDown={(e) => {
-                if (e.key === 'Enter') {
-                  toggleFilterDependencies(tech);
-                }
-              }}
+              className={`options__sort ${
+                options.sortedIn === 'Asc'
+                  ? 'options__sort--1'
+                  : 'options__sort--2'
+              }`}
+              onClick={options.sortedIn === 'Asc' ? sortDesc : sortAsc}
             >
-              {filterDependencies.includes(tech) ? '-' : '+'} {tech}
+              <span>Date</span>{' '}
+              <span className="options__sort-icon">
+                {options.sortedIn === 'Asc' ? <FaArrowUp /> : <FaArrowDown />}
+              </span>
             </button>
-          ))}
+          </div>
+          <div className="options__right">
+            <button
+              onClick={() => setFilterDependencies([])}
+              style={{
+                cursor: filterDependencies.length ? 'pointer' : 'default',
+              }}
+              className={`options__filter ${
+                filterDependencies.length
+                  ? 'options__filter--2'
+                  : 'options__filter--1'
+              }`}
+            >
+              All
+            </button>
+            {listOfPossibleTechnologies.map((tech) => (
+              <button
+                className={`options__filter ${
+                  filterDependencies.includes(tech)
+                    ? 'options__filter--1'
+                    : 'options__filter--2'
+                }`}
+                key={tech}
+                onClick={() => {
+                  toggleFilterDependencies(tech);
+                }}
+                onKeyDown={(e) => {
+                  if (e.key === 'Enter') {
+                    toggleFilterDependencies(tech);
+                  }
+                }}
+              >
+                {filterDependencies.includes(tech) ? '-' : '+'} {tech}
+              </button>
+            ))}
+          </div>
         </div>
         <ProjectsList
-          projects={projects}
+          projects={portfolio}
           options={options}
           dependencies={filterDependencies}
         />
@@ -127,4 +143,8 @@ const Portfolio = () => {
   );
 };
 
-export default Portfolio;
+const mapStateToProps = (state) => ({
+  portfolio: state.data?.portfolio,
+});
+
+export default connect(mapStateToProps)(Portfolio);
