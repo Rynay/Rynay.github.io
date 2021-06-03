33076fc3236af9f01b327cee3667a340ecb69b5d
Rynay <feirs911@gmail.com>
2021-06-03 16:05:51 +0300

    about page + firebase done

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,60 +1,55 @@
-import { FaCode, FaDatabase, FaCodeBranch } from 'react-icons/fa';
+import { connect } from 'react-redux';
+import Skeleton from 'react-loading-skeleton';
 
-const About = () => {
+const About = ({ about, skills }) => {
   return (
     <>
-      <section>
-        <h2 className="title">
-          <span>About me</span>
-        </h2>
-        <p>
-          Started earnest brother believe an exposed so. Me he believing
-          daughters if forfeited at furniture. Age again and stuff downs spoke.
-          Late hour new nay able fat each sell. Nor themselves age introduced
-          frequently use unsatiable devonshire get. They why quit gay cold rose
-          deal park. One same they four did ask busy. Reserved opinions fat him
-          nay position. Breakfast as zealously incommode do agreeable furniture.
-          One too nay led fanny allow plate.
-        </p>
-      </section>
       <section style={{ backgroundColor: 'rgb(245,245,245)' }}>
         <h2 className="title">
           <span>My Skills</span>
         </h2>
         <div className="card-container">
-          <div className="skill-card">
-            <h3>
-              <FaCode /> <span>Front-end</span>
-            </h3>
-            <ul className="list">
-              <li className="list-item">React</li>
-              <li className="list-item">Redux</li>
-              <li className="list-item">JavaScript</li>
-              <li className="list-item">HTML</li>
-              <li className="list-item">CSS</li>
-            </ul>
-          </div>
-          <div className="skill-card">
-            <h3>
-              <FaDatabase /> <span>Databases</span>
-            </h3>
-            <ul className="list">
-              <li className="list-item">Firebase</li>
-            </ul>
-          </div>
-          <div className="skill-card">
-            <h3>
-              <FaCodeBranch /> <span>Source Control</span>
-            </h3>
-            <ul className="list">
-              <li className="list-item">Git</li>
-              <li className="list-item">GitHub</li>
-            </ul>
-          </div>
+          {!skills && (
+            <>
+              <Skeleton width={'20rem'} height={'20rem'} count={1} />
+              <Skeleton width={'20rem'} height={'20rem'} count={1} />
+              <Skeleton width={'20rem'} height={'20rem'} count={1} />
+            </>
+          )}
+          {skills &&
+            skills.map((skill) => (
+              <div key={skill.title} className="skill-card">
+                <h3>
+                  <div>
+                    <img src={skill.icon} alt="icon" />
+                  </div>
+                  <span>{skill.title}</span>
+                </h3>
+                <ul className="list">
+                  {skill.list.map((item) => (
+                    <li key={item} className="list-item">
+                      {item}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ))}
         </div>
+      </section>
+      <section>
+        <h2 className="title">
+          <span>About me</span>
+        </h2>
+        {!about && <Skeleton height={'10rem'} width={'100%'} count={1} />}
+        {about && <p>{about}</p>}
       </section>
     </>
   );
 };
 
-export default About;
+const mapStateToProps = (state) => ({
+  about: state.data?.about,
+  skills: state.data?.skills,
+});
+
+export default connect(mapStateToProps)(About);
